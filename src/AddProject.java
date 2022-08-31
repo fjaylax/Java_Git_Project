a9802ce49cb90796f7ecf5380a2197ae536aa0a6
Jay Alexenko <zhmenya@yahoo.com>
2022-08-31 10:27:26 -0400

    AddProject class updated for Nadya

--- src/AddProject.java
+++ src/AddProject.java
@@ -2,4 +2,7 @@ public class AddProject {
     //need to develop and check additional futures
     //let's put together in line 3
     //line 3: all work done is here
+
+
+    //Nadya you need add another line here
 }
